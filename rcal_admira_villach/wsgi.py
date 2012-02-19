696b786812ed24333aeb1a0959ea93683aa1ae3f
Florian Apolloner <florian@apolloner.eu>
2012-02-19 13:47:54 +0100

    initial template

---
+++ rcal_admira_villach/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for rcal_admira_villach project.
+"""
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rcal_admira_villach.settings")
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(application)
