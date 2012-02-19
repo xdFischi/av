2612ef983b87ad790e73055ec897a961c2be5efc
Florian Apolloner <florian@apolloner.eu>
2012-02-19 14:02:42 +0100

    add urlconfig :)

--- rcal_admira_villach/urls.py
+++ rcal_admira_villach/urls.py
@@ -1,17 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'rcal_admira_villach.views.home', name='home'),
-    # url(r'^rcal_admira_villach/', include('rcal_admira_villach.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('rcal_registration.urls')),
+#    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^', include('rcal.urls')),
 )
