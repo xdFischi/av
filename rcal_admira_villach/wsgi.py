15b36231113d97cc70b12e5bf3db05547002a5dc
Florian Apolloner <florian@apolloner.eu>
2012-02-19 17:17:27 +0100

    add newrelic

--- rcal_admira_villach/wsgi.py
+++ rcal_admira_villach/wsgi.py
@@ -2,6 +2,7 @@
 WSGI config for rcal_admira_villach project.
 """
 import os
+import newrelic.agent
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rcal_admira_villach.settings")
 
@@ -12,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(application)
+application = newrelic.agent.wsgi_application()(StaticFilesHandler(application))
