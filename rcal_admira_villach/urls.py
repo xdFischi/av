b843bc5b6e5322e731950b017ac0adb20b9df20b
Florian Apolloner <florian@apolloner.eu>
2012-03-26 15:03:13 +0200

    don't display notification stuff in the amdin.

--- rcal_admira_villach/urls.py
+++ rcal_admira_villach/urls.py
@@ -2,6 +2,13 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
+import notification.models
+
+notification_models = ('NoticeType', 'NoticeSetting', 'Notice',
+    'NoticeQueueBatch', 'ObservedItem')
+notification_models = map(lambda x: getattr(notification.models, x),
+    notification_models)
+admin.site.unregister(notification_models)
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
