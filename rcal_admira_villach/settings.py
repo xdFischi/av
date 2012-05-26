d68cf67e403da3b8423297d3ca76ca3eabb8180f
Florian Apolloner <florian@apolloner.eu>
2012-05-26 12:46:16 +0200

    fix sentry dsn

--- rcal_admira_villach/settings.py
+++ rcal_admira_villach/settings.py
@@ -196,5 +196,5 @@ AUTH_PROFILE_MODULE = 'rcal_registration.UserProfile'
 REGISTRATION_DEFAULT_GROUP = 6
 RCAL_TIMES = (7, 21)
 
-SENTRY_DSN = 'https://e2e345bf807746389da2691cecaef7cd:4309277c6a904769a2d8f5f49e1ca03d@app.getsentry.com/489'
+SENTRY_DSN = 'https://0ad40da5e5fb4b408a16de44de8fdf46:7d711966d5c1459e802755170d1b64af@app.getsentry.com/893'
 REGISTRATION_NOTIFICATION_MAIL = 'platzreservierung-admira@gmx.at'
